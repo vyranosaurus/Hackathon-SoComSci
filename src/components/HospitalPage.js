c09c6a1bec337ef6359e1b2cd91d6b17a54122ef
RenatoCausing <rccausing@up.edu.ph>
2025-05-04 03:36:32 +0800

    gg

--- src/components/HospitalPage.js
+++ src/components/HospitalPage.js
@@ -264,9 +264,9 @@ function HospitalPage() {
                        <ArrowLeft size={20} color="white" />
                     </div>
                     <div style={{textAlign: 'center', flexGrow: 1}}>
-                         <h1 style={{color: 'black', fontSize: '1.5rem', margin: 0}}>{selectedHospital.name}</h1>
+                         <h1 style={{color: 'white', fontSize: '1.5rem', margin: 0}}>{selectedHospital.name}</h1>
                          {/* Location below name for clarity */}
-                         <p style={{color: '#ccc', fontSize: '0.9rem', margin: '5px 0 0 0'}}>{selectedHospital.location}</p>
+                         <p style={{color: 'white', fontSize: '0.9rem', margin: '5px 0 0 0'}}>{selectedHospital.location}</p>
                     </div>
                 </header>
             )}
